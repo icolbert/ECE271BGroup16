b5305378c86c99aabb88e3f4b15a7477e0acf1ea
icolbert <ian.colbert711@gmail.com>
2018-02-19 16:32:23 -0800

    Updated CNN

--- utils/models/cnn.py
+++ utils/models/cnn.py
@@ -19,30 +19,31 @@ class CNN(object):
         self.name = 'CNN'
 
     def __call__(self, x, reuse=True):
-        x = tf.reshape(x, [-1, 28, 28, 1]) # reshapes it to 28x28 images
-        conv1 = tc.layers.convolution2d(
-            x, 64, [4, 4], [2, 2],
-            weights_initializer=tf.random_normal_initializer(stddev=0.02),
-            activation_fn=tf.identity
-        )
-        conv1 = leaky_relu(conv1)
+        with tf.variable_scope(self.name, reuse=reuse):
+            x = tf.reshape(x, [-1, 28, 28, 1]) # reshapes it to 28x28 images
+            conv1 = tc.layers.convolution2d(
+                x, 64, [4, 4], [2, 2],
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                activation_fn=tf.identity
+            )
+            conv1 = leaky_relu(conv1)
 
-        conv2 = tc.layers.convolution2d(
-            conv1, 128, [4, 4], [2, 2],
-            weights_initializer=tf.random_normal_initializer(stddev=0.02),
-            activation_fn=tf.identity
-        )
-        conv2 = leaky_relu(conv2)
+            conv2 = tc.layers.convolution2d(
+                conv1, 128, [4, 4], [2, 2],
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                activation_fn=tf.identity
+            )
+            conv2 = leaky_relu(conv2)
 
-        conv2 = tcl.flatten(conv2)
-        fc1 = tc.layers.fully_connected(
-            conv2, 1024,
-            weights_initializer=tf.random_normal_initializer(stddev=0.02),
-            activation_fn=tf.identity
-        )
-        fc1 = leaky_relu(fc1)
-        fc2 = tc.layers.fully_connected(fc1, self.c_dim, activation_fn=tf.sigmoid)
-        return fc2
+            conv2 = tcl.flatten(conv2)
+            fc1 = tc.layers.fully_connected(
+                conv2, 1024,
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                activation_fn=tf.identity
+            )
+            fc1 = leaky_relu(fc1)
+            fc2 = tc.layers.fully_connected(fc1, self.c_dim, activation_fn=tf.sigmoid)
+            return fc2
 
     @property
     def vars(self):
