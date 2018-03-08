8302c0efd170073ca5f4fd26f5cca3e2009d93cc
icolbert <ian.colbert711@gmail.com>
2018-03-08 12:32:07 -0800

    Attempted to merge codes

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ if __name__ == '__main__':
 	class_labels = {str(x):x for x in range(10)}
 	class_labels.update({'+':10, 'times':11, '-':12 })
 	label_class = dict( zip(class_labels.values(), class_labels.keys() ))
-	model = LoadModel(model=args.model, class_labels=class_labels, data_ver=args.data_ver)
-	x = model.data['val'][0:2]
-	print(model(x))
+	CNN = LoadModel(model=args.model, class_labels=class_labels, data_ver=args.data_ver)
+	x = CNN.data['val'][0:2]
+	print(CNN(x))
 
 	path1 = 'data/equation-data/'
 	temp1 = np.load(path1+'Equations_images_1.npy')
@@ -49,16 +49,34 @@ if __name__ == '__main__':
 	temp4 = np.load(path1+'Equations_images_4.npy')
 	eqn_full = [temp1,temp2,temp3,temp4]
 
+	RFILE = open('cnn-results-ver{0}.txt'.format(args.data_ver))
+	for eqns in eqn_full:
+		for c in range(len(eqns)):
+			eqn1 = eqns[c]
+			segments= preprocess.extract_segments(eqn1, 40, reshape = 1, size = [28,28], 
+												area=150, gray = True, dil = True,  ker = 1)
+			for i in range(len(segments)):
+				temp = segments[i]
+				temp = temp.reshape(1,-1)
+				cnn_pred_ver1 += label_class[model.predict(temp)[0]]
+			
+			print(cnn_pred_ver1)
+
+	'''path = 'saved_models'
+	adastage1_ver1,adadigits_ver1, adachars_ver1, rfmodel_ver1, MLP_single_ver1, CNN_ver_1 = preprocess.load_models(path,1,class_labels)
+	#adastage1_ver2,adadigits_ver2, adachars_ver2, rfmodel_ver2, MLP_single_ver2, CNN_ver_2 = preprocess.load_models(path,2,class_labels)
+
 	for eqns in eqn_full:
 		for c in range(len(eqns)):
 			# initialising strings to store output of model predictions
 			rf_pred_ver1 = ''
 			ada_pred_ver1 = ''
 			mlp_pred_ver1 = ''
-			rf_pred_ver2 = ''
-			ada_pred_ver2 = ''
-			mlp_pred_ver2 = ''
+			#rf_pred_ver2 = ''
+			#ada_pred_ver2 = ''
+			#mlp_pred_ver2 = ''
 			cnn_pred_ver1 = ''
+			#cnn_pred_ver2 = ''
 
 	#         print('\nEquation = ',c)
 			eqn1 = eqns[c]
@@ -68,17 +86,32 @@ if __name__ == '__main__':
 
 			# run prediction on each segment
 			#plt.figure(figsize=(20,20))
-			'''plt.imshow(eqn1, cmap='gray')
-			plt.show()
-			print(len(segments))'''
 			for i in range(len(segments)):
 				temp = segments[i]
 				temp = temp.reshape(1,-1)
-				cnn_pred_ver1 += label_class[model.predict(temp)[0]]
-			
-			print(cnn_pred_ver1)
+				pred = preprocess.predict(temp, label_class, adastage1_ver1, 
+											adadigits_ver1, adachars_ver1, rfmodel_ver1, MLP_single_ver1, CNN=CNN_ver_1)
+				ada_pred_ver1 += pred[0] + ' '
+				rf_pred_ver1 += pred[1] + ' '
+				mlp_pred_ver1 += pred[2] + ' '
+				cnn_pred_ver1 += pred[3] + ' '
+				
+				pred = preprocess.predict(temp, label_class, adastage1_ver2, 
+											adadigits_ver2, adachars_ver2, rfmodel_ver2, MLP_single_ver2, CNN=CNN_ver_2)
+				ada_pred_ver2 += pred[0] + ' '
+				rf_pred_ver2 += pred[1] + ' '
+				mlp_pred_ver2 += pred[2] + ' '
+				cnn_pred_ver2 += pred[3] + ' '
+				
+			print('RF model_ver1 result : ',rf_pred_ver1)
+			print('adaboost_ver1 2 stage model result : ',ada_pred_ver1)
+			print('MLP_ver1 single stage model result : ',mlp_pred_ver1)
+			print('CNN_ver1 results: ', cnn_pred_ver1)
+			print('\nRF model_ver2 result : ',rf_pred_ver2)
+			print('adaboost_ver2 2 stage model result : ',ada_pred_ver2)
+			print('MLP single_ver2 stage model result : ',mlp_pred_ver2)
+			print('CNN_ver2 result: ', cnn_pred_ver2)
+			'''
 
 
 
-					
-			#plt.show()
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -16,11 +16,39 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 import keras
+from .models import TrainModel
+
+
+class LoadModel_CNN(TrainModel):
+    def __init__(self,model='cnn', **kwargs):
+        assert isinstance(model, str), "the model type must be a string, not a {0}".format(type(model))
+        
+        TrainModel.__init__(self, model=model, class_labels=kwargs.pop('class_labels', False), data_ver=kwargs.pop('data_ver', 1))
+
+    def predict(self, data, model_path='saved_models'):
+        assert (data.shape[1] == self.IN_DIM), "Need to have the same dimension data and labels as what it was trained on"
+
+        x = tf.placeholder(tf.float32, [None, self.IN_DIM])
+        logits, probs = self.model(x)
+
+        with tf.Session() as sess:
+            try:
+                tf.train.Saver().restore(sess, '{0}/{1}/{1}.ckpt'.format(model_path, self.model.name))
+                y = sess.run(probs, feed_dict={x: data})
+            except Exception as e:
+                print('Error: ', e)
+                train_model = input('Should I train a new model? [y/n] ')
+                if (train_model.lower() == 'y'):
+                  self.train()
+                  plot_acc = input('Should I plot the accuracy? [y/n] ')
+                  if (plot_acc.lower() == 'y'):
+                    self.plot_results()
+                self.sess.run(self.probs, feed_dict={self.x: data})
+        
+        return np.argmax(y, axis=1)
 
 
 # ## Image segmentation + Resampling
-
-
 def rescale_segment( segment, size = [28,28], pad = 0 ):
     '''function for resizing (scaling down) images
     input parameters
@@ -124,60 +152,64 @@ def load_data(class_labels, data_name, label_name, train=0.85, val=0.15):
     assert (_data['val'].shape[0] == _labels['val'].shape[0])
     return _data, _labels
 
-def load_models(path, ver = 1):
+def load_models(path, ver, class_labels):
     '''Function to Load trained models from given path, assuming the names of each models are known
     Inputs 
     path : path to the directory with stored models (str)
     ver : version of models loaded (1 = 20000 MNIST + HASY , 2 = 60000 MNIST + Kaggle)'''
     
     if ver == 1:
-        with open(path+'Adaboost_stage1_ver1.1.sav','rb') as f:
+        with open(path+'/Adaboost_stage1_ver1.1.sav','rb') as f:
             adastage1_ver1 = pickle.load(f)
-        with open(path+'Adaboost_digits_ver1.1.sav','rb') as f:
+        with open(path+'/Adaboost_digits_ver1.1.sav','rb') as f:
             adadigits_ver1 = pickle.load(f)
-        with open(path+'Adaboost_chars_ver1.1.sav','rb') as f:
+        with open(path+'/Adaboost_chars_ver1.1.sav','rb') as f:
             adachars_ver1 = pickle.load(f)
-        with open(path+'RFmodel_ver1.1.sav','rb') as f:
+        with open(path+'/RFmodel_ver1.1.sav','rb') as f:
             rfmodel_ver1 = pickle.load(f)
             
         # load json and create keras model
-        json_file = open(path+'MLP_singlestage_ver1.1.json', 'r')
+        json_file = open(path+'/MLP_singlestage_ver1.1.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         MLP_single_ver1 = model_from_json(loaded_model_json)
         # load weights into new model
-        MLP_single_ver1.load_weights(path+"MLP_singlestage_ver1.1.h5")
+        MLP_single_ver1.load_weights(path+"/MLP_singlestage_ver1.1.h5")
         adam1 = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
         MLP_single_ver1.compile(loss=keras.losses.categorical_crossentropy, optimizer=adam1,metrics=['accuracy'] )
         
+        CNN_ver_1 = LoadModel_CNN(model='cnn', class_labels=class_labels, data_ver=ver)
+
         print("Loaded model from disk")
-        return adastage1_ver1,adadigits_ver1, adachars_ver1, rfmodel_ver1, MLP_single_ver1
+        return adastage1_ver1,adadigits_ver1, adachars_ver1, rfmodel_ver1, MLP_single_ver1, CNN_ver_1
        
     if ver == 2:
-        with open(path+'Adaboost_stage1_ver2.1.sav','rb') as f:
+        with open(path+'/Adaboost_stage1_ver2.1.sav','rb') as f:
             adastage1_ver2 = pickle.load(f)
-        with open(path+'Adaboost_digits_ver2.1.sav','rb') as f:
+        with open(path+'/Adaboost_digits_ver2.1.sav','rb') as f:
             adadigits_ver2 = pickle.load(f)
-        with open(path+'Adaboost_chars_ver2.1.sav','rb') as f:
+        with open(path+'/Adaboost_chars_ver2.1.sav','rb') as f:
             adachars_ver2 = pickle.load(f)
-        with open(path+'RFmodel_ver2.1.sav','rb') as f:
+        with open(path+'/RFmodel_ver2.1.sav','rb') as f:
             rfmodel_ver2 = pickle.load(f)
             
         # load json and create keras model
-        json_file = open(path+'MLP_singlestage_ver2.1.json', 'r')
+        json_file = open(path+'/MLP_singlestage_ver2.1.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         MLP_single_ver2 = model_from_json(loaded_model_json)
-        MLP_single_ver2.load_weights(path+"MLP_singlestage_ver2.1.h5")
+        MLP_single_ver2.load_weights(path+"/MLP_singlestage_ver2.1.h5")
         adam1 = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
         MLP_single_ver2.compile(loss=keras.losses.categorical_crossentropy, optimizer=adam1,metrics=['accuracy'] )
 
+        CNN_ver_2 = LoadModel_CNN(model='cnn', class_labels=class_labels, data_ver=ver)
+
         print("Loaded model from disk")
-        return adastage1_ver2,adadigits_ver2, adachars_ver2, rfmodel_ver2, MLP_single_ver2
+        return adastage1_ver2,adadigits_ver2, adachars_ver2, rfmodel_ver2, MLP_single_ver2, CNN_ver_2
     
     
 def predict(temp, label_class, adastage1 = None,adadigits= None, adachars= None
-            , rfmodel= None, MLP_single= None ):
+            , rfmodel= None, MLP_single= None, CNN=None):
     '''Function for using trained models to make prediction, and return predictions
     Inputs
     temp : image (segment) of digit / symbol
@@ -203,7 +235,10 @@ def predict(temp, label_class, adastage1 = None,adadigits= None, adachars= None
     if  MLP_single :
         mlp_pred = label_class[ np.argmax(  MLP_single.predict( temp ) ) ] 
         predictions.append(mlp_pred)
-                           
+    
+    if CNN:
+        predictions.append(label_class[CNN.predict(temp)[0]])
+
     return predictions
     
     
